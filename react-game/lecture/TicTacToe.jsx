7cd3e81a7b0bc632baf59e58474198b1d7ee9cdb
Woobeen906 <siugan@naver.com>
2022-07-12 23:31:04 +0900

    ADD : TicTacToe 기본틀

---
+++ react-game/lecture/TicTacToe.jsx
@@ -0,0 +1,40 @@
+import React, {useCallback, useReducer, useState} from 'react';
+import Table from "./Table";
+
+const initialState = {
+    winner: '',
+    turn: 'O',
+    tableData: [['', '', ''], ['', '', ''], ['', '', '']],
+};
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case 'SET_WINNER':
+            //  state.winner = action.winner; 이렇게 하면 안됨.
+            return{
+                ...state,
+                winner:action.winner,
+            }
+    }
+};
+
+const TicTacToe = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // const [winner, setWinner] = useState();
+    // const [turn, setTurn] = useState('O');
+    // const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+
+    const onClickTable = useCallback(() => {
+        dispatch({type: 'SET_WINNER', winner: 'O'});
+    }, []);
+
+    return (
+        <>
+            <Table onClick={onClickTable}/>
+            {state.winner && <div>{state.winner}님의 승리</div>}
+        </>
+    );
+};
+
+export default TicTacToe;
